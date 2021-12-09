9e11a5ce72c67611d4a49e158c7c1e4a921aec1b
Md. Tahmidur Rafid <amtrafid@gmail.com>
2021-12-10 00:00:20 +0600

    Update

--- 1605046.py
+++ 1605046.py
@@ -8,14 +8,19 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 
-# csvData = pandas.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
-csvData = pandas.read_csv("test.csv")
+csvData = pandas.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+# csvData = pandas.read_csv("test.csv")
 X = csvData.iloc[:, :-1].values
 Y = csvData.iloc[:, -1].values
 labelencoder = preprocessing.LabelEncoder()
 Y = labelencoder.fit_transform(Y)
+Y[Y == 0] = -1
 ct = ColumnTransformer([("cols", OneHotEncoder(drop='if_binary'), [1, 2])], remainder = 'passthrough')
 X = ct.fit_transform(X)
 sc_X = preprocessing.StandardScaler()
 X = sc_X.fit_transform(X)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+print(X)
+print(Y)
+
